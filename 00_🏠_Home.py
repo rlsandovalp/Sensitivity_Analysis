68dcde40798339dba0a57822a4abf220a32a64a6
Leonardo <rlsandovalp@unal.edu.co>
2023-02-02 16:23:42 +0100

    bbb

--- 00_🏠_Home.py
+++ 00_🏠_Home.py
@@ -6,7 +6,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Sensitivity Analysis",
     page_icon="📈",
-    layout="centered",
+    layout="wide",
     initial_sidebar_state="expanded"
 )
 
@@ -20,4 +20,8 @@ To illustrate:
 - What additional products you can get with the information required for SA
 - Example 1 - Ishigami Function
 - Example 2 - Gas flow in low permeable materials (Under construction)
+
+The theory and examples are taken from:
+- Dell'Oca, A., Riva, M. and Guadagnini, A., 2017. Moment-based metrics for global sensitivity analysis of hydrological systems. Hydrology and Earth System Sciences, 21(12), pp.6219-6234. https://doi.org/10.5194/hess-21-6219-2017
+- Sandoval, L., Riva, M., Colombo, I. and Guadagnini, A., 2022. Sensitivity Analysis and Quantification of the Role of Governing Transport Mechanisms and Parameters in a Gas Flow Model for Low-Permeability Porous Media. Transport in Porous Media, 142(3), pp.509-530. https://doi.org/10.1007/s11242-022-01755-x
 """
